57d6eea2f01cb0cfec1d47e6b3c4dfc97aaefe38
Kody agin peters <131360013+KodKod22@users.noreply.github.com>
2025-06-25 09:39:28 +0300

    creating the task modul and api router

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,10 +1,20 @@
 // Require mongoose
-
+const mongoose = require('mongoose');
+const { addTask } = require('../controllers/dbController');
 
 // Implement schema for a task
 // Each task should have:
 // - title (String, required)
 // - done (Boolean, defaults to false)
-
-
+const newSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    done: {
+        type: Boolean,
+        default: false
+    }
+});
 // Export the model as 'Task'
+module.exports = mongoose.model('Task',newSchema);
